c24cfe0bc56f2bed00514c52b178528eaceae542
Ioan Rusu <ioanrazvan@gmail.com>
2019-11-03 00:14:20 +0200

    Added first laboratory homework

--- Lab homeworks/Lab 2 homework/RGB_CONTROLLER/RGB_CONTROLLER.ino
+++ Lab homeworks/Lab 2 homework/RGB_CONTROLLER/RGB_CONTROLLER.ino
@@ -2,37 +2,44 @@ const int analogInputPin0 = A0;
 const int analogInputPin1 = A1;
 const int analogInputPin2 = A2;
 
-const int analogOutputPin11 = 11;
+const int analogOutputPin9 = 9;
 const int analogOutputPin10 = 10;
-//const int analogOutputPin;
+const int analogOutputPin11 = 11;
+
+int potValueOutputPin9;
+int redLedValue;
 
 int potValueOutputPin10;
-int ledValueOutputPin10;
+int greenLedValue;
 
 int potValueOutputPin11;
-int ledValueOutputPin11;
-
-//int potValue;
-//int ledValue;
+int blueLedValue;
 
 void setup() {
-  Serial.begin(9600);
   
   pinMode(analogInputPin0, INPUT);
   pinMode(analogInputPin1, INPUT);
+  pinMode(analogInputPin2, INPUT);
   
   pinMode(analogOutputPin10, OUTPUT);
   pinMode(analogOutputPin11, OUTPUT);
+  pinMode(analogOutputPin9, OUTPUT);
+  
 }
 
 void loop() {
-  potValueOutputPin10 = analogRead(analogInputPin0);
-  ledValueOutputPin10 = map(potValueOutputPin10, 0, 1023, 0, 255);
   
-  potValueOutputPin11 = analogRead(analogInputPin1);
-  ledValueOutputPin11 = map(potValueOutpuPin11, 0, 1023, 0, 255);
+  potValueOutputPin11 = analogRead(analogInputPin0);
+  redLedValue = map(potValueOutputPin11, 0, 1023, 0, 255);
   
-  analogWrite(analogOutputPin11, ledValue1);
-  analogWrite(analogOutputPin10, ledValue0);
- 
+  potValueOutputPin10 = analogRead(analogInputPin1);
+  greenLedValue = map(potValueOutputPin10, 0, 1023, 0, 255);
+
+  potValueOutputPin9 = analogRead(analogInputPin2);
+  blueLedValue = map(potValueOutputPin9, 0, 1023, 0, 255);
+  
+  analogWrite(analogOutputPin11, redLedValue);
+  analogWrite(analogOutputPin10, greenLedValue);
+  analogWrite(analogOutputPin9, blueLedValue);
+  
 }
